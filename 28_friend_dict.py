389cf537d1b7661b9d9f3875ee47e14b9d91928c
Dhrumil Patel <98255642+itechdp@users.noreply.github.com>
2023-10-28 11:54:13 +0530

    28_friend_dict.py

--- 28_friend_dict.py
+++ 28_friend_dict.py
@@ -1,47 +1,33 @@
-# fav = {}
-# a = input("enter your fav language")
-# b = input("enter your fav language")
-# c = input("enter your fav language")
-# d = input("enter your fav language")
+fav = {}
+a = input("enter your fav language")
+b = input("enter your fav language")
+c = input("enter your fav language")
+d = input("enter your fav language")
 
-# fav["dhrumil"] = a
-# fav["rian"] = b
-# fav["fannie"] = c
-# fav["krishna"] = d
+fav["dhrumil"] = a
+fav["rian"] = b
+fav["fannie"] = c
+fav["krishna"] = d
 
-# print(type(fav))
+print(type(fav))
 
-# print(fav.items())
+print(fav.items())
 
-# when the key are with the same name it will overwrite the value of a key
+when the key are with the same name it will overwrite the value of a key
 
-# to prove that un-comment below code
+to prove that un-comment below code
 
-# fav = {}
-# a = input("enter your fav language")
-# b = input("enter your fav language")
-# c = input("enter your fav language")
-# d = input("enter your fav language")
+fav = {}
+a = input("enter your fav language")
+b = input("enter your fav language")
+c = input("enter your fav language")
+d = input("enter your fav language")
 
-# fav["dhrumil"] = a
-# fav["fannie"] = b
-# fav["fannie"] = c
-# fav["krishna"] = d
+fav["dhrumil"] = a
+fav["fannie"] = b
+fav["fannie"] = c
+fav["krishna"] = d
 
-# print(type(fav))
+print(type(fav))
 
-# print(fav.items())
-
-from tkinter.font import names
-
-
-name= {}
-
-for i in range (2):
-    b = input("first name:")
-    a = input('lastname:')
-    name.update({b:a})
-   
-
-print(name)
-
+print(fav.items())
