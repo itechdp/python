c82f8991d1555fce27c696579074aaab8f7fbf34
Dhrumil Patel <98255642+itechdp@users.noreply.github.com>
2023-10-28 11:51:38 +0530

    26_word_dictionery.py

--- 26_word_dictionery.py
+++ 26_word_dictionery.py
@@ -1,26 +1,11 @@
-# hindiengdict = {
+hindiengdict = {
 
-#     "hoshiyar" : "intteligent",
-#     "sukh" : "happy",
-#     "badla hua" : "changed"
+    "hoshiyar" : "intteligent",
+    "sukh" : "happy",
+    "badla hua" : "changed"
 
-# }
+}
 
-# print(hindiengdict.keys())
-# a = input("Enter your hindi word: ")
-# print(hindiengdict[a])
-
-contact = {
-
- }
-
-for i in range(0,3):
-
-    name = input("enter name:")
-    num = input("enter number:")
-    print("")
-
-contact = {name.title() : num}
-
-
-print(contact)
+print(hindiengdict.keys())
+a = input("Enter your hindi word: ")
+print(hindiengdict[a])
