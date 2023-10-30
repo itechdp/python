41878a3d13b61841e1c741edee7aa00336fbd909
Dhrumil Patel <98255642+itechdp@users.noreply.github.com>
2023-10-30 18:22:40 +0530

    23_dictionery_methods.py

--- 23_dictionery_methods.py
+++ 23_dictionery_methods.py
@@ -4,15 +4,15 @@ a = {
     "krishna": "loves football"
 }
 
-# use this syntax to get the value of the entered key in the print funcion
+# use this syntax to get the value of the entered key in the print function
 
 print(a["dhrumil"])
 
-# .items() used get the key values of the dictionery
+# .items() used to get the key values of the dictionary
 
 print(a.items())
 
-# .keys() to get the list of keys of the dictionery
+# .keys() to get the list of keys of the dictionary
 
 print(a.keys())
 
@@ -22,7 +22,7 @@ a.update({"mummy": "loves cooking"})
 
 print(a)
 
-# .get() get the value of the entered key without thowing the error
+# .get() Get the value of the entered key without throwing the error
 
 print(a.get("mummy"))
 
@@ -41,14 +41,14 @@ print(dict1)
 #used to get the values of the dictionary
 print(dict1.values())
 
-#used to update the dictionary or append the list taking argument in {} braces and taking key value pairs 
-dict1.update({'fannie':30})
+#used to update the dictionary or append the list taking argument in {} braces and taking key-value pairs 
+dict1.update({'rian':30})
 print(dict1)
 
-#used to delete the last inserted key value pair from the dictionary
+#used to delete the last inserted key-value pair from the dictionary
 dict1.popitem()
 print(dict1)
 
-#used to remove perticular element of the dictionary taking argument as an index value
+#used to remove the particular dictionary element taking argument as an index value
 dict1.pop(1)
 print(dict1)
